f31e419a27b17922cf8948b05f85112ab0416c43
Laszlo Toth <TothLaszlo2@otpbank.hu>
2021-05-26 09:27:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+parse_trans_problem_app2
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/parse_trans_problem_app2_app.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%% @doc parse_trans_problem_app2 public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(parse_trans_problem_app2_app).
+
+-compile([{parse_transform, parse_trans_problem_pt}]).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    parse_trans_problem_app2_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
